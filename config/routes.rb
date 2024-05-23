6a8b70d78da80bc54e48b94f5871d3c69190c916
Mitch Matthews <165244888+mitchmatthews@users.noreply.github.com>
2024-05-23 04:58:27 +0000

    finished graded assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get("/", { :controller => "pages", :action => "home"})
 
+  get("/dice/:number_of_dice/:how_many_sides", {:controller => "game", :action => "flexible"})
 end
